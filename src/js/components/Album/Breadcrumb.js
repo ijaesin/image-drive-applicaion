11c2bc02dcc8488c6ac2d1dcec2173b40fdb7bb5
ijaesin <ijaesinii@gmail.com>
2023-03-21 17:18:27 +0900

    Create breadcrumb component.

---
+++ src/js/components/Album/Breadcrumb.js
@@ -0,0 +1,66 @@
+import Component from "../../helpers/component.js";
+
+class Breadcrumb extends Component {
+  constructor(props) {
+    super(props);
+    const { parentElement } = props;
+    this.routes = [{ nane: "ROOT" }];
+    this.parentElement = parentElement;
+    this.renderElement = Breadcrumb.createBreadcrumb();
+    this.init();
+  }
+
+  static createBreadcrumb() {
+    const breadcrumbWrapper = document.createElement("section");
+    const breadcrumb = document.createElement("div");
+    const backButton = document.createElement("button");
+
+    breadcrumbWrapper.classList.add("breadcrumb-container");
+    breadcrumb.classList.add("breadcrumbs");
+    backButton.classList.add("button-back");
+
+    breadcrumbWrapper.appendChild(breadcrumb);
+    breadcrumbWrapper.appendChild(backButton);
+
+    return breadcrumbWrapper;
+  }
+
+  init() {
+    this.parentElement.appendChild(this.renderElement);
+    this.bindEvents();
+  }
+
+  bindEvents() {
+    const backButton = this.renderElement.querySelector(".button-back");
+    backButton.addEventListener("click", () => this.emit("back"));
+  }
+
+  forward(route) {
+    this.routes.push(route);
+
+    return this;
+  }
+
+  back() {
+    this.routes.pop();
+    return this;
+  }
+
+  getParentNode() {
+    return this.routes(this.routes.length - 1);
+  }
+
+  render() {
+    const routeElements = this.routes
+      .map((route) => {
+        `<span>${route.name}</span>`;
+      })
+      .join("");
+
+    this.renderElement.querySelector(".breadcrumbs").innerHTML = routeElements;
+
+    return this.renderElement;
+  }
+}
+
+export default Breadcrumb;
--- src/js/components/ImageViewer/ImageViewer.js
+++ src/js/components/ImageViewer/ImageViewer.js
@@ -1,13 +1,40 @@
 class ImageViewer {
-  constructor() {}
+  constructor(props) {
+    const { parentElement } = props;
+    this.parentElement = parentElement;
+    this.renderElement = ImageViewer.createImageViewer();
+    this.bindEvents();
+  }
 
-  static createImageViewer() {}
+  static createImageViewer() {
+    const imageViewerWrapper = document.createElement("section");
+    const imageContent = document.createElement("div");
+    const imageElement = document.createElement("img");
 
-  bindEvents() {}
+    imageViewerWrapper.classList.add("modal", "image-viewer");
+    imageContent.classList.add("content");
 
-  open() {}
+    imageContent.appendChild(imageElement);
+    imageViewerWrapper.appendChild(imageContent);
 
-  close() {}
+    return imageViewerWrapper;
+  }
+
+  bindEvents() {
+    this.renderElement.addEventListener("click", (event) => {
+      const currentTarget = event.target;
+      if (this.renderElement === currentTarget) this.close();
+    });
+  }
+
+  open(filePath = "") {
+    this.renderElement.querySelector("img").src = filePath;
+    this.parentElement.appendChild(this.renderElement);
+  }
+
+  close() {
+    this.parentElement.removeChild(this.renderElement);
+  }
 }
 
 export default ImageViewer;
